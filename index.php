7d47f489dcc0d5ea85df5e7d41bbc66e7505c3ef
gulimadev <limacloudsis@gmail.com>
2023-04-17 21:51:33 -0300

    add css index

--- index.php
+++ index.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<title>Minha Loja de Alimentos</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    
+	<header>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- add bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<h1>Minha Loja de Alimentos</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Produtos</a></li>
+				<li><a href="#">Sobre Nós</a></li>
+				<li><a href="#">Contato</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<h2>Produtos em Destaque</h2>
+		<div class="col-md-4 produto">
+			<img src="img/arroz.png" alt="Arroz">
+			<h3>Arroz Branco</h3>
+			<p>R$ 5,00</p>
+			<a href="#" class="botao-comprar">Comprar</a>
+		</div>
+		<div class="col-md-4 produto">
+			<img src="img/feijao.jpg" alt="Feijão">
+			<h3>Feijão Carioca</h3>
+			<p>R$ 6,00</p>
+			<a href="#" class="botao-comprar">Comprar</a>
+		</div>
+		<div class="col-md-4 produto">
+			<img src="img/acucar.webp" alt="Açúcar">
+			<h3>Açúcar Refinado</h3>
+			<p>R$ 4,00</p>
+			<a href="#" class="botao-comprar">Comprar</a>
+		</div>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 Minha Loja de Alimentos</p>
+	</footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>

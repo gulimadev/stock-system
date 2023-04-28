f508a1ccd90061aead56f65b6d3227b2d9329d65
wildgodrenato <wildgodrenato@gmail.com>
2023-04-27 23:56:57 -0300

    ok

--- admin.php
+++ admin.php
@@ -26,62 +26,16 @@ $produtos = listarProdutos();
     <link rel="stylesheet" type="text/css" href="style.css">
   </head> 
 <!-- Formulário para adicionar um produto -->
-<<<<<<< HEAD
-  <body>
-    <form method="post">
-      <input type="hidden" name="acao" value="adicionar">
-      <input type="text" name="nome" placeholder="Nome">
-      <input type="number" name="preco" placeholder="Preço">
-      <input type="file" name="imagem">
-=======
 <form method="POST">
   <input type="hidden" name="acao" value="adicionar">
   <input type="text" name="nome" placeholder="Nome">
   <input type="number" step="any" name="preco" placeholder="Preço">
   <input type="file" name="imagem" >
->>>>>>> Renato
 
       <input type="number" name="quantidade" placeholder="Quantidade">
       <button type="submit">Adicionar</button>
     </form>
 
-<<<<<<< HEAD
-    <!-- Tabela com a lista de produtos -->
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Preço</th>
-          <th>Imagem</th>
-          <th>Quantidade</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($produtos as $produto): ?>
-        <tr>
-          <td><?= $produto['id'] ?></td>
-          <td><?= $produto['nome'] ?></td>
-          <td><?= $produto['preco'] ?></td>
-          <td><?= $produto['imagem'] ?></td>
-          <td><?= $produto['quantidade'] ?></td>
-          <td>
-            <form method="post">
-              <input type="hidden" name="acao" value="deletar">
-              <input type="hidden" name="id" value="<?= $produto['id'] ?>">
-              <button type="submit">Deletar</button>
-            </form>
-            <form method="post">
-      <input type="hidden" name="acao" value="atualizar">
-      <input type="hidden" name="id" value="<?= $produto['id'] ?>">
-      <input type="text" name="nome" placeholder="Novo nome">
-      <input type="number" name="preco" placeholder="Novo preço">
-      <input type="hidden" name="imagem" value="<?= $produto['imagem'] ?>">
-      <input type="hidden" name="quantidade" value="<?= $produto['quantidade'] ?>">
-      <button type="submit">Atualizar</button>
-    </form>
-=======
 <!-- Tabela com a lista de produtos -->
 <table>
   <thead>
@@ -117,7 +71,6 @@ $produtos = listarProdutos();
   <input type="hidden" name="quantidade" value="<?= $produto['quantidade'] ?>">
   <button type="submit">Atualizar</button>
 </form>
->>>>>>> Renato
 
           </td>
         </tr>

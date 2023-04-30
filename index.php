ac097b9fcc1e1a0782994dfad986fb00ebd9dc1f
Gulimadev <limacloudsis@gmail.com>
2023-04-29 22:25:00 -0300

    add carrol

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Minha Loja de ALimentos</title>
     <link rel="stylesheet" type="text/css" href="./bootstrap/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src=".js/script.js"> </script>
 </head>
 <body>
 	<header>
@@ -20,8 +21,30 @@
 		</nav>
 	</header>
 
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="5000">
+        <img src="img/img1.png" class="d-block w-100" alt="Imagem 1">
+      </div>
+      <div class="carousel-item" data-bs-interval="5000">
+        <img src="img/img2.png" class="d-block w-100" alt="Imagem 2">
+      </div>
+      <div class="carousel-item" data-bs-interval="5000">
+        <img src="img/img3.png" class="d-block w-100" alt="Imagem 3">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Anterior</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Próximo</span>
+    </button>
+  </div>
+
 	<main>
-  <h2>Produtos em Destaque</h2>
+      <h2>Produtos em Destaque</h2>
   <div class="row">
     <?php
       // Conecta ao banco de dados
@@ -52,5 +75,9 @@
 	<footer>
 		<p>&copy; 2023 Minha Loja de Alimentos</p>
 	</footer>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="./bootstrap/bootstrap.js"></script>
+    <script src=".js/script.js"> </script>
 </body>
 </html>
